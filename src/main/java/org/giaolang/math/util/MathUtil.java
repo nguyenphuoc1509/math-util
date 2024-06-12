2f75195dfe0f4c69ea23a98fe52253313cfd098b
nguyenphuoc1509 <nguyenphuoc150903@gmail.com>
2024-06-12 17:08:25 +0700

    modify main code to use recursion & to see green 4ever again

--- src/main/java/org/giaolang/math/util/MathUtil.java
+++ src/main/java/org/giaolang/math/util/MathUtil.java
@@ -21,12 +21,26 @@ public class MathUtil {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
         }
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n - 1);
+        //n! = n * (n - 1)!  //stack LIFO - last in first out
+        //USING RECURSION TO DEMONSTRATE REGRESSION TEST
     }
 }
+
+//HỌC VỀ REGRESSION TEST - KIỂM THỬ HỒI QUY
+//KIỂM THỬ LẠI NHỮNG THỨ ĐÃ TỪNG KIỂM THỬ. TẠI SAO PHẢI LÀM ĐIỀU NÀY
+//LÍ DO: CODE ĐANG ỔN, MÀ KO ĐỤNG VÀO NÓ, THÌ NÓ VẪN ỔN
+//      NHƯNG NẾU CÓ FIX BUG, CÓ SỬA HÀM, CÓ TỐI ƯU THUẬT TOÁN TRONG HÀM, THÌ
+//      HÀM CẦN PHẢI DC TEST LẠI - TEST LẠI THỨ ĐÃ TỪNG TEST 
+//      TEST LẠI HÀM ĐÃ TỪNG TEST, ĐỂ XÁC NHẬN RẰNG NÓ CÒN NGON - CÒN XANH HAY KO SAU KHI CODE ĐC SỬA
+
+//NẾU CÓ UNIT TEST (JUNIT) VIỆC TEST LẠI CODE CỰC NHANH
+//CHỈ CẦN THẤY MÀU XANH CỦA CÁC JUNIT TEST CASE ĐC RUN LÀ ĐỦ 
+//NHANH NỮA: ĐƯA UNIT TEST LÊN GITHUB, CÀI CI SCRIPT (FILE YAML YML) VÀO MỌI THỨ TỰ ĐỘNG BÀO XANH ĐỎ LUÔN
+
+//KIỂM THỬ LẠI NHỮNG THỨ ĐÃ KIỂM THỬ GỌI LÀ TEST HỒI QUY - REGRESSION TEST
+//NÊN DÙNG TỰ ĐỌNG BẰNG CÁCH XÀI UNIT TEST FRAMEWORK
+//NHÌN = MẮT TỪ HÀM MAIN() CÁCH TRUYỀN THỐNG LÀ KO NÊN
