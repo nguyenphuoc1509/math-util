10b726104001257ed88dd7ca39104028869a7a66
NguyenPhuoc1509 <153739242+nguyenphuoc1509@users.noreply.github.com>
2024-06-12 16:00:52 +0700

    update main class directly in server to demonstrate the confliction

--- src/main/java/org/giaolang/math/util/main/Main.java
+++ src/main/java/org/giaolang/math/util/main/Main.java
@@ -9,6 +9,12 @@ package org.giaolang.math.util.main;
  * @author DELL
  */
 public class Main {
+
+    //this method is written in directly in server to simulate
+    //the work of another team member, he uploads code before me
+    public static void SayHiFromServer() {
+        
+    }
     public static void main(String[] args) {
         int n = 0;
         long expectedResult = 1;
