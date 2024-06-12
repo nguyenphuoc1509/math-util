7b5c01f8e5182c8280819071867ce4098344ac4a
nguyenphuoc1509 <nguyenphuoc150903@gmail.com>
2024-06-12 16:12:10 +0700

    update main class in local

--- src/main/java/org/giaolang/math/util/main/Main.java
+++ src/main/java/org/giaolang/math/util/main/Main.java
@@ -9,6 +9,10 @@ package org.giaolang.math.util.main;
  * @author DELL
  */
 public class Main {
+    //this method is written in local
+    public static void SayHiFromLocal() {
+        
+    }
     public static void main(String[] args) {
         int n = 0;
         long expectedResult = 1;
